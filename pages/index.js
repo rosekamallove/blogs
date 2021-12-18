6776230af6e4d563def152c78e8a0c9239973a2d
Rose Kamal Love <69139607+rosekamallove@users.noreply.github.com>
2021-12-18 18:40:52 +0530

    render posts on home page

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,10 @@
 import Head from "next/head";
-import Link from "next/link";
-import utilStyles from "../components/utils.module.css";
 import Layout, { siteTitle } from "../components/layout";
-import { getSortedPostData } from "../lib/posts";
+import utilStyles from "../components/utils.module.css";
+import { getSortedPostsData } from "../lib/posts";
 
 export async function getStaticProps() {
-  const allPostsData = getSortedPostData();
+  const allPostsData = getSortedPostsData();
   return {
     props: {
       allPostsData,
@@ -13,7 +12,7 @@ export async function getStaticProps() {
   };
 }
 
-export default function Home() {
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -43,6 +42,20 @@ export default function Home() {
           to build this simple but website with an effective learning
           experience)
         </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   );
