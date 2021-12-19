da43a535723687a8d3e1c12182a1f3cd90a65d57
Rose Kamal Love <69139607+rosekamallove@users.noreply.github.com>
2021-12-19 11:41:53 +0530

    getStaticPaths for [id].js

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,4 +1,14 @@
 import Layout from "../../components/layout";
+import { getAllPostIds } from "../../lib/posts";
+
+export async function getStaticPaths() {
+  // Return a list of possible value for id
+  const paths = getAllPostIds();
+  return {
+    paths,
+    fallback: false,
+  };
+}
 
 export default function Post() {
   return <Layout></Layout>;
